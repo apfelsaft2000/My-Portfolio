5a1aa3d9c156d72fc7c4ac28bad41741f3a7944e
apfelsaft2000 <apfelhouse@icloud.com>
2023-03-20 23:44:03 +0900

    first commit

---
+++ reactailwind/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "standard-with-typescript",
+    "prettier",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  root: true,
+  rules: {},
+};
